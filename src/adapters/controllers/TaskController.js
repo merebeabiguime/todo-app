d53ca82d69619fef005a374efd935c40cba39298
Merebe <merebe2012@gmail.com>
2024-03-04 17:47:05 +0100

    Use Interactor inside each controller's function

--- src/adapters/controllers/TaskController.js
+++ src/adapters/controllers/TaskController.js
@@ -2,9 +2,62 @@ export class TaskController {
   constructor(taskInteractor) {
     this._taskInteractor = taskInteractor;
   }
-  async onCreateTask(req, res) {}
-  async onUpdateTask(req, res) {}
-  async onDeleteTask(req, res) {}
-  async onGetTaskList(req, res) {}
-  async onGetTask(req, res) {}
+  async onCreateTask(req, res) {
+    try {
+      const body = req.body;
+
+      //Validation before sending to Interactor
+
+      const data = await this._taskInteractor.createTask(body);
+      return res.status(200).json(data);
+    } catch (error) {
+      //error handling
+    }
+  }
+  async onUpdateTask(req, res) {
+    try {
+      const body = req.body;
+
+      //Validation before sending to Interactor
+
+      const data = await this._taskInteractor.updateTask(body);
+      return res.status(200).json(data);
+    } catch (error) {
+      //error handling
+    }
+  }
+  async onDeleteTask(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+
+      //Validation before sending to Interactor
+
+      const data = await this._taskInteractor.updateTask(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      //error handling
+    }
+  }
+  async onGetTaskList(req, res) {
+    try {
+      //Validation before sending to Interactor
+
+      const data = await this._taskInteractor.getTaskList();
+      return res.status(200).json(data);
+    } catch (error) {
+      //error handling
+    }
+  }
+  async onGetTask(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+
+      //Validation before sending to Interactor
+
+      const data = await this._taskInteractor.getTask(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      //error handling
+    }
+  }
 }
