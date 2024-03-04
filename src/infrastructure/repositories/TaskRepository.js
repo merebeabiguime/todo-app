2e3a4988e2a2df9665c0dfd853d1f570dc98e21b
Merebe <merebe2012@gmail.com>
2024-03-04 22:45:06 +0100

    Writing all Repository functions

--- src/infrastructure/repositories/TaskRepository.js
+++ src/infrastructure/repositories/TaskRepository.js
@@ -7,8 +7,9 @@ export class TaskRepository {
   }
 
   async create(body) {
+    //Transforming the body to a valid json array for sql query
     const taskDTOarray = TaskDTO.toArray(body);
-    //Mapping through body to dynamically create the request based on the DTO
+    //Mapping through body to dynamically create the sql request based on the DTO
     const columns = Object.keys(body).join(", ");
     const placeholders = Object.keys(body)
       .map(() => "?")
@@ -17,9 +18,35 @@ export class TaskRepository {
       `INSERT INTO task (${columns}) VALUES (${placeholders})`,
       taskDTOarray
     );
+    return rows[0];
   }
-  update(body) {}
-  find(id) {}
-  getAll() {}
-  delete(id) {}
+  async update(body) {
+    //Transforming the body to a valid json array for sql query
+    const taskDTOarray = TaskDTO.toArray(body);
+    //Adding id at the end for the WHERE clause
+    taskDTOarray.push(body.id);
+    // Mapping through body to dynamically create the SET part of the SQL query
+    const sets = Object.keys(body)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+    const [rows] = await _database.query(
+      `UPDATE task SET ${sets} WHERE id = ?`,
+      taskDTOarray
+    );
+    return rows[0];
+  }
+  async find(id) {
+    const [rows] = await _database.query(`SELECT * FROM task WHERE id = ?`, [
+      id,
+    ]);
+    return rows[0];
+  }
+  async getAll() {
+    const [rows] = await _database.query(`SELECT * FROM task`);
+    return rows;
+  }
+  async delete(id) {
+    const [rows] = await _database.query(`DELETE FROM task WHERE id = ?`, [id]);
+    return rows[0];
+  }
 }
